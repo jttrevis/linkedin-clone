390a59442a71d3bb0f8121396b21843c4c5e1708
jttrevis <jr.trevis@hotmail.com>
2023-01-31 17:59:02 +0000

    added feed section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 
@@ -9,6 +10,7 @@ function App() {
 
 			<div className='app__body'>
 				<Sidebar />
+				<Feed />
 			</div>
 		</div>
 	);
---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,46 @@
+import './Feed.css';
+import CreateIcon from '@mui/icons-material/Create';
+import InputOptions from './../InputOptions/InputOptions';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+const Feed = () => {
+	return (
+		<div className='feed'>
+			<div className='feed__inputContainer'>
+				<div className='feed__input'>
+					<CreateIcon />
+					<form className=''>
+						<input type='text' />
+						<button type='submit'>Send</button>
+					</form>
+				</div>
+				<div className='feed__inputOptions'>
+					<InputOptions
+						Icon={ImageIcon}
+						title='Photo'
+						color='#70b5f9'
+					/>
+					<InputOptions
+						Icon={SubscriptionsIcon}
+						title='Video'
+						color='#e7a33e'
+					/>
+					<InputOptions
+						Icon={EventNoteIcon}
+						title='Event'
+						color='#c0cbcd'
+					/>
+					<InputOptions
+						Icon={CalendarViewDayIcon}
+						title='Write article'
+						color='#7fc15e'
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Feed;
